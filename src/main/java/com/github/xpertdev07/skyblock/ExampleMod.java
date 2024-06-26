9177a08afa21ea38bf22bc6f00adc67ee114540c
GitHub Action <action@github.com>
2024-06-26 14:46:17 +0000

    Initialize template repository proper
    Set package name to com.github.xpertdev07.skyblock
    Set project name to skyblock
    Set modid to skyblock

--- src/main/java/com/github/xpertdev07/skyblock/ExampleMod.java
+++ src/main/java/com/github/xpertdev07/skyblock/ExampleMod.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.github.xpertdev07.skyblock;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "skyblock", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
